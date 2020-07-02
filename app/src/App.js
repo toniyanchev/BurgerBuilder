1f5e763eb2e343922ad254b5c41d7d94770be750
toniyanchev <anton.t.y2002@gmail.com>
2020-07-02 17:32:16 +0300

    Course examples1

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Burger from './Burger/Burger';
 
 class App extends Component {
+  state = {
+    burgers: [
+      { type: "chicken", price: 3.80 },
+      { type: "beef", price: 5.50 },
+      { type: "chicken", price: 5.30 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello Burger!</h1>
+        <Burger type={this.state.burgers[0].type} price={this.state.burgers[0].price} />
+        <Burger type={this.state.burgers[1].type} price={this.state.burgers[1].price} />
+        <Burger type={this.state.burgers[2].type} price={this.state.burgers[2].price} />
       </div>
     );
   }
---
+++ app/src/Burger/Burger.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const burger = (props) => {
+	return <p>I am {props.type} burger and cost {props.price} leva.</p>
+};
+
+export default burger;
