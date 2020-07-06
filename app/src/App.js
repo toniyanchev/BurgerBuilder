ea6c8f957ae14073616002a2c8255924c9558472
toniyanchev <anton.t.y2002@gmail.com>
2020-07-06 13:57:49 +0300

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,104 @@
 import React, { Component } from 'react';
 import './App.css';
 import Burger from './Burger/Burger';
+import Radium from 'radium';
 
 class App extends Component {
+
   state = {
     burgers: [
-      { type: "chicken", price: 3.80 },
-      { type: "beef", price: 5.50 },
-      { type: "chicken", price: 5.30 }
-    ]
-  };
+      { id:"qwer", type: "veggie", price: 3.80 },
+      { id:"asdf", type: "beef", price: 5.50 },
+      { id:"zxcv", type: "chicken", price: 5.30 }
+    ],
+    showBurgers: false
+  }
+
+  typeChangeHandler = ( event, id ) => {
+    const burgerIndex = this.state.burgers.findIndex(b => {
+      return b.id === id;
+    });
+
+    const burger = {
+      ...this.state.burgers[burgerIndex]
+    };
+
+    burger.name = event.target.value;
+
+    const burgers = [...this.state.burgers];
+    burgers[burgerIndex] = burger;
+
+    this.setState({burgers: burgers});
+  }
+
+  deleteBurgerHandler = (burgerIndex) => {
+    const burgers = [...this.state.burgers];
+    burgers.splice(burgerIndex, 1);
+    this.setState({burgers: burgers});
+  }
+
+  toggleBurgersHandler = (event) => {
+    const doesShow = this.state.showBurgers;
+    this.setState({
+      showBurgers: !doesShow
+    });
+  }
 
   render() {
+
+    const style = {
+    backgroundColor: 'green',
+    border: '2.5px solid black',
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: 'lightgreen'
+    }
+    };
+
+    const assignesClasses = [];
+    if (this.state.burgers.length <= 2) {
+      assignesClasses.push('red');
+    }
+    if (this.state.burgers.length <= 1) {
+      assignesClasses.push('bold');
+    }
+
+    let burgers = null;
+
+    if (this.state.showBurgers) {
+      burgers = (
+        <div>
+        {this.state.burgers.map((burger, index) => {
+          return <Burger
+            click={() => this.deleteBurgerHandler(index)}
+            type={burger.type}  
+            price={burger.price}
+            key={burger.id}
+            changed={(event) => this.typeChangeHandler(event, burger.id)} />
+        })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'tomato'
+      }
+    }
+
     return (
       <div className="App">
-        <h1>Hello Burger!</h1>
-        <Burger type={this.state.burgers[0].type} price={this.state.burgers[0].price} />
-        <Burger type={this.state.burgers[1].type} price={this.state.burgers[1].price} />
-        <Burger type={this.state.burgers[2].type} price={this.state.burgers[2].price} />
+        <h1 className={assignesClasses.join(' ')}>Hello Burger!</h1>
+
+        <button
+        onClick={this.toggleBurgersHandler}
+        style={style}
+        >Toggle burgers</button>
+
+        {burgers}
+
       </div>
+
     );
   }
-}
+};
 
-export default App;
+export default Radium(App);
--- app/src/Burger/Burger.js
+++ app/src/Burger/Burger.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const burger = (props) => {
-	return <p>I am {props.type} burger and cost {props.price} leva.</p>
+import './Burger.css';
+
+const burger = ( props ) => {
+	return (
+		<div className="Burger">
+			<p onClick={props.click}>I am {props.type} burger and cost {props.price} leva.</p>
+			<input type="text" onChange={props.changed} value={props.type}/>
+		</div>
+	)
 };
 
 export default burger;
