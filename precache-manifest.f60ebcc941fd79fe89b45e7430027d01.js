7916c13662cedc990b4ad283084a01432ae7be92
toniyanchev <anton.t.y2002@gmail.com>
2020-07-10 09:04:44 +0300

    Updates

---
+++ precache-manifest.f60ebcc941fd79fe89b45e7430027d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c2f60de2dab29eb486cedcf1ed49f9",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "fe30c58d35c6ce6a1013",
+    "url": "/BurgerBuilder/static/css/main.6e07aeaa.chunk.css"
+  },
+  {
+    "revision": "603a89434a762caf72cb",
+    "url": "/BurgerBuilder/static/js/2.bbdcf6cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder/static/js/2.bbdcf6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe30c58d35c6ce6a1013",
+    "url": "/BurgerBuilder/static/js/main.4b1faa05.chunk.js"
+  },
+  {
+    "revision": "8066b1b1028ec28b0856",
+    "url": "/BurgerBuilder/static/js/runtime-main.9fa8d4ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
